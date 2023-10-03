265b84da51711f59777e97e321e4e00a379cc773
Piotr Groblewski <groblewskip@gmail.com>
2023-10-03 16:51:48 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# React Video Player with React + Vite
+
+code source/guide: 
+
+https://code.pieces.app/blog/react-player-customized-controls
---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import ReactPlayer from 'react-player';
+import {Container} from '@mui/material';
+
+
+
+
+function App() {
+  
+  return (
+    <> 
+    
+    </>
+  )
+}
+
+export default App
